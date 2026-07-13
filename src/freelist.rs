8757fa410a8af147afb551580545c010addfb129
Hamza <official.hamzaderaoui@gmail.com>
2026-07-13 15:15:27 +0100

    Implement safe and unsafe slot accessors in freelist

--- src/freelist.rs
+++ src/freelist.rs
@@ -17,4 +17,22 @@ impl FreeList {
     fn is_null(&self) -> bool {
         self.head.is_null()
     }
+    pub fn get_slot(&mut self) -> Option<*mut u8> {
+        if !self.is_null() {
+            let current = self.head;
+            let next = unsafe {
+                *(self.head as *mut *mut u8) /* ptr to the first 8 bytes before deref */
+            }; // after deref read first 8 bytes
+            self.head = next as *mut u8;
+            return Some(current);
+        }
+        None
+    }
+    unsafe fn get_slot_unchecked(&mut self) -> *mut u8 {
+        let current = self.head;
+        // will case SegFault. this used in testing only
+        let next = unsafe { *(self.head as *mut *mut u8) };
+        self.head = next as *mut u8;
+        current
+    }
 }
