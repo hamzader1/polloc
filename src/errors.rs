8dbb0f02f3e258d246dcdb71770673268c5d3ba8
Hamza <official.hamzaderaoui@gmail.com>
2026-07-13 14:29:08 +0100

    Add AllocErr enum with variants for allocation errors

---
+++ src/errors.rs
@@ -0,0 +1,31 @@
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum AllocErr {
+    Overflow,
+    OutOfMemory,
+    InvalidAlignment,
+    DoubleFree,
+}
+
+impl fmt::Display for AllocErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            AllocErr::Overflow => {
+                write!(f, "allocation size overflow")
+            }
+            AllocErr::OutOfMemory => {
+                write!(f, "out of memory")
+            }
+            AllocErr::InvalidAlignment => {
+                write!(f, "invalid memory alignment")
+            }
+            AllocErr::DoubleFree => {
+                write!(f, "attempted to free memory more than once")
+            }
+        }
+    }
+}
+
+impl Error for AllocErr {}
